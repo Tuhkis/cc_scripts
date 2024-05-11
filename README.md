c07cbacee36b0b2473ca366cc5ca076cf3ab4674
Tuhkis <nicolindstromj@gmail.com>
2024-05-11 22:28:26 +0300

    Remove attack surface from lock

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 My programs for Computer Craft, a Minecraft mod. Everything in this repo is CC0. Use as you wish.
 
 ## Lock
-A lock program that requires the user to log in. Only requires the Computer Craft mod. Add the program to your startup and write a `.passwd`-file in your root. You may also write `.locksplash` to change the top text of the program.
+A lock program that requires the user to log in. Only requires the Computer Craft mod. Add the program to your startup and write a `.passwd`-file in your root. The password cannot be longer than 64 characters, and should not contain newlines. You may also write `.locksplash` to change the top text of the program.
--- lock.lua
+++ lock.lua
@@ -5,18 +5,27 @@ os.pullEvent = os.pullEventRaw
 local w, h = term.getSize()
 local instruction = 'Type your password.'
 local y_middle = h * 0.5
-
 local guess = ''
+local run = true
 
 -- get password
-local f = fs.open('.passwd', 'r')
+local password
+local f = fs.open('/.passwd', 'r')
 if f == nil then
   print('No password set in ".passwd"')
+  print('Press a key to continue...')
   read()
-  os.reboot()
+  run = false
+else
+  password = f.readLine()
+  f.close()
+  if #password > 64 then
+    print('Dangerously long password. A password cannot be longer than 64 characters.')
+    print('Press a key to continue...')
+    read()
+    run = false
+  end
 end
-local password = f.readLine()
-f.close()
 
 -- get splash text.
 local splash = 'lock by Tuhkis'
@@ -26,7 +35,7 @@ if fs.exists('.locksplash') then
   f.close()
 end
 
-while true do
+while run do
   term.clear()
   term.setCursorPos(w * 0.5 - #instruction * 0.5, y_middle)
   term.setTextColor(colors.yellow)
@@ -44,16 +53,17 @@ while true do
     guess = guess:sub(1, -2)
   elseif c == keys.enter then
     if guess == password then
+      run = false
       break
     else
       guess = ''
     end
   else
     local e1, c1 = os.pullEvent('char')
-    guess = guess .. c1
+    if #guess < 64 then
+      guess = guess .. c1
+    end
   end
-    
-  sleep(1 / 22)
 end
 term.clear()
 term.setCursorPos(1, 1)
